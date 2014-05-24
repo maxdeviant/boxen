5aab8a63bdf0fa05b72eb7f822345ce196e661c8
Marshall Bowers <maxdeviant@fuxing.local>
2014-05-23 21:40:49 -0400

    Add Alfred.

--- manifests/site.pp
+++ manifests/site.pp
@@ -58,6 +58,9 @@ node default {
   include hub
   include nginx
 
+  # custom modules
+  include alfred
+
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
